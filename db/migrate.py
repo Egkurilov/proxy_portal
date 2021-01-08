094515e48d4c5619c7b25a249afad4ecfc3d1c10
Shq <egkurilov@gmail.com>
2021-01-08 13:02:18 +0300

    add stop

--- db/migrate.py
+++ db/migrate.py
@@ -13,7 +13,7 @@ class ProxyList(Base):
     id = Column(Integer, primary_key=True)
     proxy_pid = Column(String(2500))
     project = Column(String(2500), nullable=False, default="Имя проекта.")
-    proxy_port_in = Column(Integer, nullable=False, default=32000, onupdate=32000)
+    proxy_port_in = Column(Integer, nullable=False, default=32000, server_onupdate="32000")
     proxy_name = Column(String(2500), nullable=False)
     fp_name = Column(String(2500), nullable=False)
     author = Column(String(2500), nullable=False)
